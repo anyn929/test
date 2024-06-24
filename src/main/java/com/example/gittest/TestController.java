f3d5de234f40b5c33cbb4e34d985a7a27827cdf7
DESKTOP-RQ85CUE\安雅楠 <10211009@sxqc.com>
2024-06-24 11:05:30 +0800

    test01 new brunch

---
+++ src/main/java/com/example/gittest/TestController.java
@@ -0,0 +1,7 @@
+package com.example.gittest;
+
+import com.sun.org.apache.xerces.internal.impl.xpath.XPath;
+
+public class TestController {
+    public static final String WORD_TEST = "wordTest";
+}
